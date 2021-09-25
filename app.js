2453abe7566e08ab4ac9f43f936e88a60a958ca2
aryelh <aryelh@gmail.com>
2021-09-25 17:35:35 -0300

    Primer commit proyecto mercado liebre

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');//unifica las rutas en los distintos S.O.para que express identifique las rutas origen destino
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+//la variable dirname hace referencia a la ubicacion deonde nos encontramos
+const publicPath = path.resolve(__dirname, './public');//le decimos a path que resuelva la ruta que se le pasa
+app.use( express.static(publicPath) );//le decimos a express que queremos tener la carpeta public como recurso de archivos estaticos
+
+app.get('/',(req, res)=> {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+app.get('/login',(req, res)=> {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+app.get('/register',(req, res)=> {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
