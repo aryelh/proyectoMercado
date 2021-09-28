953ea0f2350fd071e6a8423a842937dd784d2dc1
aryelh <aryelh@gmail.com>
2021-09-28 19:39:56 -0300

    creados procfile y rutas

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 //corriendo el servidor
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log('Server running on port 3000', 'http://localhost:3000');
 });
 
